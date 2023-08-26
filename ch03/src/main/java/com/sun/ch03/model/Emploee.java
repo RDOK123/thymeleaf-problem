bd6ff33c677aa6c83c19f005381d4cddf0e09636
KORD <jjjoy1745@gmail.com>
2023-08-26 10:25:32 +0800

    0826

--- ch03/src/main/java/com/sun/ch03/model/Emploee.java
+++ ch03/src/main/java/com/sun/ch03/model/Emploee.java
@@ -1,12 +1,25 @@
 package com.sun.ch03.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@AllArgsConstructor
-@Data
-@NoArgsConstructor
 public class Emploee {
     private String name;
+
+    // 無參數建構子
+    public Emploee() {
+    }
+
+    // 全參數建構子
+    public Emploee(String name) {
+        this.name = name;
+    }
+
+    // getter for name
+    public String getName() {
+        return name;
+    }
+
+    // setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
